dcb4cb7b52d498dae0114836454a98c0ded7ce74
okayu <moka1988jp@gmail.com>
2019-11-11 23:39:33 +0900

    TypeScript + webpack導入

---
+++ src/audio-spectrum.ts
@@ -0,0 +1,58 @@
+console.log('init');
+
+// const canvas = document.querySelector('#canvas');
+// const canvasContext = canvas.getContext('2d');
+// const fftSize = 128;
+// canvas.width = 512;
+// canvas.height = 300;
+
+// function render(spectrum) {
+//   const barWidth = canvas.width / spectrum.length;
+//   const barHeight = 300
+//   canvasContext.fillStyle = 0x323232;
+//   canvasContext.clearRect(0, 0, canvas.width, canvas.height);
+//   for (let i = 0; i < spectrum.length; i++) {
+//     canvasContext.fillRect(barWidth * i, canvas.height - spectrum[i], barWidth, canvas.height);
+//   }
+// }
+
+// let sound = window.sound = new Howl({
+//   src: "./audio.mp3",
+//   // src: "./bgm003.ogg",
+//   audoplay: false
+// });
+// let first = true;
+
+// document.querySelector("#play").addEventListener('click', async (event) => {
+//   if (first) {
+//     const audioContext = Howler.ctx;
+//     const analyserNode = audioContext.createAnalyser(); // 音分析ノード
+//     analyserNode.fftSize = fftSize;
+
+//     // スペクトラムを保持するUint8Array
+//     // サイズはfftSizeの半分（＝frequencyBinCount）
+//     const spectrumArray = new Uint8Array(analyserNode.frequencyBinCount);
+
+//     sound.on('play', () => {
+//       // console.log(sound._sounds[0]._node.bufferSource);
+//       const audioSource = sound._sounds[0]._node.bufferSource;
+//       audioSource.connect(analyserNode);
+//       // すでにhowler.jsで出力ノードには接続されているので↓は不要
+//       // analyserNode.connect(audioContext.destination);
+
+//       setInterval((event) => {
+//         analyserNode.getByteFrequencyData(spectrumArray);
+//         // analyserNode.getByteTimeDomainData(spectrumArray);
+//         render(spectrumArray);
+//       }, 1 / 60);
+//     });
+
+//   }
+//   first = false;
+//   sound.play();
+// });
+
+// document.querySelector("#stop").addEventListener('click', () => {
+//   sound.stop();
+// });
+
---
+++ webpack.config.dev.js
@@ -0,0 +1,47 @@
+const path = require('path')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const WriteFilePlugin = require('write-file-webpack-plugin');
+
+const devServerHost = process.env.WEBPACK_DEV_SERVER_HOST || '0.0.0.0';
+const devServerPort = process.env.WEBPACK_DEV_SERVER_PORT || 8080;
+
+module.exports = {
+  mode: "development",
+  entry: {
+    'audio-spectrum': path.join(__dirname, 'src/audio-spectrum.ts')
+  },
+  output: {
+    path: path.join(__dirname, 'dist_dev'),
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      { enforce: "pre", test: /\.ts$/, loader: "eslint-loader", exclude: /node_modules/ },
+      { test: /\.ts$/, loader:'ts-loader', exclude: /node_modules/ }
+    ]
+  },
+  resolve: {
+    extensions:['.ts', '.js', '.json']
+  },
+  plugins: [
+    new CopyWebpackPlugin(
+      [ { from: '.', to: '.', ignore: '!*.html' }, ],
+      { context: path.join(__dirname, 'src') }
+    ),
+    new CopyWebpackPlugin(
+      [ { from: '.', to: '.', ignore: '!*.mp3' }, ],
+      { context: path.join(__dirname, 'src') }
+    ),
+    new CopyWebpackPlugin(
+      [ { from: '.', to: '.', ignore: '!*.ogg' }, ],
+      { context: path.join(__dirname, 'src') }
+    ),
+    new WriteFilePlugin(),
+  ],
+  devServer: {
+    host: devServerHost,
+    port: devServerPort,
+    disableHostCheck: true,
+    contentBase: path.join(__dirname, 'dist_dev')
+  }
+}
