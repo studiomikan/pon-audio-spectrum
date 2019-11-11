38afc25169a40fff482c8197a00661ff15d867c0
okayu <moka1988jp@gmail.com>
2019-11-11 22:53:39 +0900

    音量が倍だったを修正

--- docs/audio-spectrum.js
+++ docs/audio-spectrum.js
@@ -2,12 +2,13 @@ console.log('init');
 
 const canvas = document.querySelector('#canvas');
 const canvasContext = canvas.getContext('2d');
-const barWidth = 10;
-canvas.width = barWidth * 64;
-canvas.height = 255;
+const fftSize = 128;
+canvas.width = 512;
+canvas.height = 300;
 
 function render(spectrum) {
-  canvas.width = barWidth * spectrum.length;
+  const barWidth = canvas.width / spectrum.length;
+  const barHeight = 300
   canvasContext.fillStyle = 0x323232;
   canvasContext.clearRect(0, 0, canvas.width, canvas.height);
   for (let i = 0; i < spectrum.length; i++) {
@@ -17,6 +18,7 @@ function render(spectrum) {
 
 let sound = window.sound = new Howl({
   src: "./audio.mp3",
+  // src: "./bgm003.ogg",
   audoplay: false
 });
 let first = true;
@@ -25,7 +27,7 @@ document.querySelector("#play").addEventListener('click', async (event) => {
   if (first) {
     const audioContext = Howler.ctx;
     const analyserNode = audioContext.createAnalyser(); // 音分析ノード
-    analyserNode.fftSize = 128;
+    analyserNode.fftSize = fftSize;
 
     // スペクトラムを保持するUint8Array
     // サイズはfftSizeの半分（＝frequencyBinCount）
@@ -35,11 +37,12 @@ document.querySelector("#play").addEventListener('click', async (event) => {
       // console.log(sound._sounds[0]._node.bufferSource);
       const audioSource = sound._sounds[0]._node.bufferSource;
       audioSource.connect(analyserNode);
-      analyserNode.connect(audioContext.destination);
-      console.log("audioSource", audioSource)
+      // すでにhowler.jsで出力ノードには接続されているので↓は不要
+      // analyserNode.connect(audioContext.destination);
 
       setInterval((event) => {
         analyserNode.getByteFrequencyData(spectrumArray);
+        // analyserNode.getByteTimeDomainData(spectrumArray);
         render(spectrumArray);
       }, 1 / 60);
     });
